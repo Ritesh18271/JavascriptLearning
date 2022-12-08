99525b805c40150b7e42db76bbe5591ada96113d
Ritesh18271 <riteshkamble240@gmail.com>
2022-12-08 10:39:41 +0530

    First ever program of java
    First ever program of java

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName //variable declaration
+myName="Ritesh" //variable initialization
+console.log(myName)
+
+var myFrdName="Sachin Tendulkar"
+console.log(myFrdName);
+
+var myFrdName="MS Dhoni"
+console.log(myFrdName)
